2c49b3106789b145b796703f8f576028349d6167
ravi_kiran <121532336+ravikirank29@users.noreply.github.com>
2026-07-23 12:37:37 -0400

    Update test_env.py

--- scripts/test_env.py
+++ scripts/test_env.py
@@ -1,11 +1,19 @@
+"""
+Environment variable tests.
+"""
+
 from config import (
     API_KEY,
     SPLUNK_HOST,
+    SPLUNK_PASSWORD,
     SPLUNK_PORT,
     SPLUNK_USERNAME,
 )
 
-print("AbuseIPDB Key:", API_KEY[:8] + "...")
-print("Splunk Host:", SPLUNK_HOST)
-print("Splunk Port:", SPLUNK_PORT)
-print("Splunk User:", SPLUNK_USERNAME)
+
+def test_environment_loaded():
+    assert API_KEY
+    assert SPLUNK_HOST
+    assert SPLUNK_PORT
+    assert SPLUNK_USERNAME
+    assert SPLUNK_PASSWORD
